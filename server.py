771f815a4f0f6558c636c8541c4c129908139c8b
elad3283@gmail.com <zxcvvcxz!123>
2017-04-06 14:46:39 +0300

    wrote the server class procedurs names

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import socket
 
-def main():
-    """
-    Add Documentation here
-    """
-    pass  # Add Your Code Here
+class Server:
 
+    def __init__(self, ip, port):
+        self.ip__ = ip
+        self.port__ = port
+        print 'starting server'
 
-if __name__ == '__main__':
-    main()
+    def open_server(self, ip, port):
+        pass
+
+    def client_connection(self, ip, port):
+        pass
+
+    def orders_loop(self):
+        pass
+
+    def override_request(self, ip, port):
+        pass
+
+    def clinets_manager(self):
+        pass
