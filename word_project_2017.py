361a1afc767342f968ead23c01ddcf725d292ce0
elad3283@gmail.com <zxcvvcxz!123>
2017-04-06 13:48:27 +0300

    added documents class procedurs

--- word_project_2017.py
+++ word_project_2017.py
@@ -1,8 +1,46 @@
 # -*- coding: utf-8 -*-
+from docx import Document
+
+class Documents:
+
+    def save_doc(self, path):
+        pass
+
+    def create_new_doc(self):
+        pass
+
+    def import_existing_doc(self, path):
+        pass
+
+    def add_new_picture(self, width):
+        pass
+
+    def add_new_table(self, rows, lines):
+        pass
+
+    def write_to_table(self, line, text, style, color):
+        pass
+
+    def add_new_paragraph(self,text, style, color):
+        pass
+
+    def add_new_word(self, text, style, color):
+        pass
+
+    def add_new_heading(self, text, style, color):
+        pass
+
+    def add_new_page_break(self):
+        pass
+
+    def email_doc(self, from_email, to_email):
+        pass
+
+    def in_doc_option_menu(self):
+        pass
+
+    def out_doc_option_menu(self):
+        pass
 
 
-def main():
-    print 'kwsjfgbhskjwhdgfkusjhdgfikusdgfiuykshdgfukiysgufgug'
 
-if __name__ == '__main__':
-    main()
