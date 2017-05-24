25e74758bfd0126c0e28a82c125c51ca1ac22f80
elad-hayek <elad3283@gmail.com>
2017-05-24 20:37:10 +0300

    need to write minings

---
+++ word-project-2017/word-project-2017/word-project-2017-ori-ex/document_class.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+from docx import Document
+from docx.shared import Pt
+from docx.shared import RGBColor
+from docx.shared import Inches
+import os
+import mail_sending
+from colorama import Fore
+import subprocess
+
+
+TABLE_STYLE = 'Table Grid'
+CMD_WORD_PROCESS = 'C:\Program Files\Microsoft Office\Office14\WINWORD.EXE'
+error_messages = {
+    'save doc': (Fore.LIGHTRED_EX, 'ERROR: path does not exist, action terminated'),
+    'add new picture': (Fore.LIGHTRED_EX, 'ERROR: path does not exist, action terminated'),
+    'write to table': ((Fore.LIGHTRED_EX, 'ERROR: there is no such row number, action terminated'),
+                      (Fore.LIGHTRED_EX, 'ERROR: there is no such column number, action terminated'),
+                      (Fore.LIGHTRED_EX, 'ERROR: there is no table to edit, action terminated')),
+    'add new paragraph': ((Fore.LIGHTRED_EX, 'ERROR: there is no such color, action terminated'),
+                          (Fore.LIGHTRED_EX, 'ERROR: there is not such style, action terminated'),
+                          (Fore.LIGHTRED_EX, 'ERROR: the font size is invalid, action terminated')),
+    'import existing doc': (Fore.LIGHTRED_EX, 'ERROR: path does not exist, action terminated'),
+    'return to last save': (Fore.LIGHTRED_EX, 'WARNING: your document was not previously saved, action terminated'),
+    'email doc': (Fore.LIGHTRED_EX, 'ERROR: your document was not previously saved, action terminated')}
+
+confirm_messages = {
+    'save doc': (Fore.LIGHTGREEN_EX, 'document saved successfully'),
+    'add new picture': (Fore.LIGHTGREEN_EX, 'picture added successfully'),
+    'add new table': (Fore.LIGHTGREEN_EX, 'table added successfully'),
+    'write to table': (Fore.LIGHTGREEN_EX, 'successfully writen to the table'),
+    'add new paragraph': (Fore.LIGHTGREEN_EX,  'paragraph added successfully'),
+    'import existing doc': (Fore.LIGHTGREEN_EX, 'successfully imported a document'),
+    'add new heading': (Fore.LIGHTGREEN_EX, 'heading added successfully'),
+    'add new page break': (Fore.LIGHTGREEN_EX, 'new page break added successfully'),
+    'email doc': (Fore.LIGHTGREEN_EX, 'your email has been sent')}
+
+colors = {'R': (0xff, 0x0, 0x0), 'G': (0x0, 0xff, 0x0), 'b': (0x0, 0x0, 0xff), 'B': (0x0, 0x0, 0x0)} #red, green, blue, black
+
+
+
+
+
+
+class Documents():
+    def __init__(self):
+        self.__path = ''
+        self.__document = Document()
+        self.__document.save('beta.docx')
+        self.__table = None
+        self.__name = ''
+
+
+    def save_doc(self, args_list):
+        path = args_list[0]
+        name = args_list[1]
+        if not os.path.exists(path):
+            return error_messages['save doc']
+        self.__path = path + '\\' + name + '.docx'
+        print 'saving document'
+        print path
+        self.__document.save(path + '\\' + name + '.docx')
+        os.remove('beta.docx')
+        self.__name = name
+        return confirm_messages['save doc']
+
+
+    def add_new_picture(self, args_list):
+        path = args_list[0]
+        width = args_list[1]
+        if not os.path.exists(path):
+            return error_messages['add new picture']
+        self.__document.add_picture(path, width=Inches(float(width)))
+        return confirm_messages['add new picture']
+
+    def add_new_table(self, args_list):
+        rows = args_list[0]
+        lines = args_list[1]
+        table = self.__document.add_table(rows=int(rows), cols=int(lines))
+        table.style = TABLE_STYLE
+        self.__table = table
+        return confirm_messages['add new table']
+
+
+
+    def write_to_table(self, args_list):
+        row = args_list[0]
+        column = args_list[1]
+        text = args_list[2]
+        print len(self.__table.rows), 'table rows'
+        print len(self.__table.columns), 'table colums'
+        if not self.__table:
+            return error_messages['write to table'][2]
+        if len(self.__table.rows) <= int(row):
+            return error_messages['write to table'][0]
+        if len(self.__table.columns) <= int(column):
+            return error_messages['write to table'][1]
+        cell = self.__table.cell(int(row), int(column))
+        cell.text = text
+        return confirm_messages['write to table']
+
+    def add_new_paragraph(self, args_list):
+        text = args_list[0]
+        style = args_list[1]
+        color = args_list[2]
+        if color not in colors:
+            return error_messages['add new paragraph'][0]
+        size = args_list[3]
+        if not 1 <= int(size) <= 1638:
+            print 'wrong size'
+            return error_messages['add new paragraph'][2]
+        font_name = args_list[4]
+        run = self.__document.add_paragraph().add_run(text)
+        font = run.font
+        font.name = font_name
+        font.color.rgb = RGBColor(colors[color][0], colors[color][1], colors[color][2])
+        font.size = Pt(int(size))
+        if style == 'B':
+            font.bold = True
+        elif style == 'I':
+            font.italic = True
+        elif style == 'N':
+            pass
+        else:
+            return error_messages['add new paragraph'][1]
+        return confirm_messages['add new paragraph']
+
+
+
+    def import_existing_doc(self, args_list):
+        path = args_list[0]
+        if not os.path.exists(path):
+            return error_messages['import existing doc']
+        self.__document = Document(path)
+        return confirm_messages['import existing doc']
+
+    def add_new_heading(self, args_list):
+        text = args_list[0]
+        self.__document.add_heading(text, level=0)
+        return confirm_messages['add new heading']
+
+    def add_new_page_break(self, args_list):
+        print 'adding new page break'
+        self.__document.add_page_break()
+        return confirm_messages['add new page break']
+
+    def return_to_last_save(self, args_list):
+        if os.path.exists('beta.docx'):
+             return error_messages['return to last save']
+        self.__document = Document(self.__path)
+        return confirm_messages['return to last save']
+
+    def live_docx(self, args_list):
+        # todo: call windocx.exe <path to file>
+
+        if os.path.exists('beta.docx'):
+            self.__document.save('beta.docx')
+            cmd_file_path = r'C:\Users\elad\Documents\GitHub\word-project-2017-ori-ex\beta.docx'
+            cmd = [CMD_WORD_PROCESS, cmd_file_path]
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            process.wait()
+        else:
+            cmd = [CMD_WORD_PROCESS, self.__path]
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            process.wait()
+        return Fore.LIGHTGREEN_EX, 'THIS IS YOUR FILE TEXT'
+
+
+    def email_doc(self, args_list):
+        if os.path.exists('beta.docx'):
+            return error_messages['email doc']
+        to_email = args_list[0]
+        mail_sending.send(to_email, self.__path, self.__name)
+        return confirm_messages['email doc']
+
+
+
+
---
+++ word-project-2017/word-project-2017/word-project-2017-ori-ex/word_project_2017.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+DOCX_SQL_FILE_NAME = 'docx_funcs.sql'
+CREATE_TABLE = '''CREATE TABLE DOCX_METHODS
+(NAME TEXT PRIMARY KEY    NOT NULL,
+ARGS_NUMBER INT     NOT NULL,
+FIRST_ARG TEXT, SECOND_ARG TEXT, THIRD_ARG TEXT, FOURTH_ARG TEXT, FIFTH_ARG TEXT, SIXTH_ARG TEXT)'''
+
+docx_methods_sql_values = [
+    "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('write to table', 2,'cell index(row,column)','cell text', null, null, null, null )",
+     "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('save doc', 2, 'saving path', 'document name', null, null, null, null )",
+      "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('add new picture',2,'the path of the picture', 'width(in inches)(note that 1 inch=25.4mm)',null, null, null, null )",
+       "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('add new table' ,2,'number of rows', 'number of columns', null, null, null, null )",
+        "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('add new paragraph', 5, 'paragraph text', 'font style(N=none, B=bold, I=italic)', 'font color(B=black, b=blue, G=green, R=red)', 'font size(from 1 to 1638)', 'Word font name \ your own font name', null )",
+          "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('add new heading' ,1 , 'heading text', null, null, null, null, null )",
+           "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('add new page break', 0, null, null, null, null, null, null )",
+            "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('email doc', 1, 'your email', null, null, null, null, null )",
+             "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('import existing doc', 1, 'document path', null, null, null, null, null )",
+              "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('return to last save', 0, null, null, null, null, null, null )",
+               "INSERT INTO DOCX_METHODS (NAME,ARGS_NUMBER,FIRST_ARG,SECOND_ARG,THIRD_ARG, FOURTH_ARG, FIFTH_ARG, SIXTH_ARG) \
+      VALUES ('live docx', 0, null, null, null, null, null, null )"]
+
+def main():
+    conn = sqlite3.connect(DOCX_SQL_FILE_NAME)
+    conn.execute(CREATE_TABLE)
+    for new_insert in docx_methods_sql_values:
+        conn.execute(new_insert)
+    conn.commit()
+    cursor = conn.execute("SELECT name, args_number, first_arg, second_arg, third_arg, fourth_arg, fifth_arg, sixth_arg from DOCX_METHODS ")
+    for row in cursor:
+        print "NAME = ", row[0]
+        print "ARGS_NUMBER = ", row[1]
+        print "FIRST_ARG = ", row[2]
+        print "SECOND_ARG = ", row[3]
+        print "THIRD_ARG = ", row[4]
+        print "FOURTH_ARG = ", row[5]
+        print "FIFTH_ARG = ", row[6]
+        print "SIXTH_ARG = ", row[7], "\n"
+    cursor = conn.execute("SELECT args_number FROM DOCX_METHODS WHERE name = 'add_new_page_break' or name = 'email_doc' ")
+    for raw in cursor:
+        print raw[0]
+
+    conn.close()
+
+if __name__ == '__main__':
+    main()
